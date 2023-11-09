3574af0986ae36e0772939010b88f869d0376700
Naveenn789 <naveengalla77@gmail.com>
2023-11-09 15:48:06 +0530

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Naveen";
+            int age = 21;
+            double height = 174.20;
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.ReadKey();
+        }
+    }
+}
